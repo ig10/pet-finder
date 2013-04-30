2adcb49b57c7a60ada528b23c6716f9c294b9afe
Ignacio Diez <ig.diez10@gmail.com>
2013-04-29 21:59:36 -0400

    Creacion de Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetFinder::Application.config.secret_token = 'e89fddedc7093e2576fb12597115ee440831cb1d12667ea2ded232275b3675fb43a5065a271869db7fdec5d6dc2dcc5f6c126b0e2cc2f4d2f4add503283476a8'
