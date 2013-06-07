6471bd97bd7e4b9ada1fde75a88fe701adfe7441
Ignacio Diez <ig.diez10@gmail.com>
2013-06-07 00:59:39 -0400

    App: Se agrega captura de 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def pagina_no_encontrada
+    raise ActionController::RoutingError.new('Oops Pagina no Encontrada')
+  end
 end
