3bbd0b3512576a98026e39bef94b1e00c4611bb3
Ignacio Diez <ig.diez10@gmail.com>
2013-06-06 23:26:42 -0400

    JS: Se reemplaza public.js por home.js y se arregla funcionalidad para envio de formulario

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,52 @@
+var Home = (function(){
+  function init(){
+    configurar_modal_registro();
+  }
+
+  function configurar_modal_registro(){
+    /*
+      This function provides the Modal window
+      configuration for Register
+    */
+    $j('#registro').on('click',function(e){
+      e.preventDefault();
+      $j.ajax({
+        url: '/nuevo_usuario',
+        type: 'GET',
+        data: {tipo: 'usuario'},
+        success: function(data) {
+          $j('#container').append(data);
+          $j('#modal_registro').modal();
+          configurar_registro();
+        },
+        error: function() {
+          console.log("ERROR!");
+        }
+      });
+
+    });
+  }
+
+  function configurar_registro(){
+    /*
+      This function, configures the Send
+      method of the Register form in the
+      Modal window.
+    */
+    $j('#formulario_registro').on('submit',function(e){
+      e.preventDefault();
+      var form = $j(this);
+      $j.post(form.attr('action'), form.serialize(), function(data){
+        if(data == "OK"){
+          $j('#modal_registro').modal('hide');
+        }else{
+          $j('#modal_registro').append("ERROR!");
+        }
+      });
+    });
+  }
+
+  return {
+    init: init
+  };
+})();
