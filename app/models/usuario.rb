c6a41bd4376787d786e97280cdbaa6dc9ea28802
Ignacio Diez <ig.diez10@gmail.com>
2013-06-23 16:04:57 -0400

    Usuario: Cambio de nombre a Callback y metodo de encriptacion de password para reutilizarla

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -6,10 +6,18 @@ class Usuario < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 5}, on: :create
   validates_uniqueness_of :correo_electronico
 
-  before_save :encriptar_password, if: Proc.new{|p| p.correo_electronico.present?}
+  before_save :formatear_password, if: Proc.new{|p| p.correo_electronico.present?}
 
-  def encriptar_password
-    self.password = Digest::SHA1.hexdigest(self.password)
+  def formatear_password
+    self.password = encriptar_password(self.password)
+  end
+
+  def self.encriptar_password(password)
+    Digest::SHA1.hexdigest(password)
+  end
+
+  def self.autenticar(correo_electronico, password)
+    Usuario.where(correo_electronico: correo_electronico, password: encriptar_password(password)).count > 0
   end
 
 end
