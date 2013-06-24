090b72d0dc8c6f3c213996490bfdaf70487abec8
Ignacio Diez <ig.diez10@gmail.com>
2013-06-24 14:11:40 -0400

    Routes: Se agregan rutas para logout, buscar e index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 PetFinder::Application.routes.draw do
   root :to => 'public#index'
 
+  match '/index' => 'public#index'
   match '/tipo-ingreso' => 'public#tipo_ingreso'
   match '/login' => 'public#login'
+  match '/logout' => 'public#logout'
   match '/registro' => 'public#registro'
-  match '/buscar' => 'public#buscar'
+
+  match '/buscar' => 'buscar#index'
 
 end
