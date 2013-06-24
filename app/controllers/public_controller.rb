67609340fd3bac6116204eb98ae15e1837f78368
Alvaro A <alv.ale.alvarez@gmail.com>
2013-06-23 22:25:08 -0400

    Se agregan redirecciones especificas para registro login Se agregan rutas

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,16 +4,37 @@ class PublicController < ApplicationController
     @session = true
   end
 
-  def nuevo_usuario
+  def tipo_ingreso
     unless request.post?
-      render partial: 'registro', locals: {perfil: params[:perfil]}
+      render partial: 'registro-login', locals: {perfil: params[:perfil], tipo_ingreso: params[:tipo_ingreso]}
     else
-      msg = Usuario.create(params[:usuario]) ? "OK" : "ERROR"
-      render text: msg
+      redirect_to action: params[:tipo_ingreso], usuario: params[:usuario]      
+    end
+  end
+
+  def registro
+    usuario = Usuario.create(params[:usuario])
+    unless usuario.blank?
+      redirect_to action: :login, usuario: usuario
+    else
+      flash[:error] = "Error al registrar"
+      redirect_to action: :index
     end
   end
 
   def login
+    usuario = true
+    if params[:usuario].is_a?(Hash)
+      usuario = Usuario.autenticar(params[:usuario])
+    end 
+    unless usuario 
+      flash[:error] = "Usuario no encontrado"
+      redirect_to action: :index
+    end
+    Usuario.crear_session(usuario) 
+    redirect_to action: :buscar 
   end
 
+  def buscar
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PetFinder::Application.routes.draw do
   root :to => 'public#index'
 
-  match '/nuevo_usuario' => 'public#nuevo_usuario'
+  match '/tipo-ingreso' => 'public#tipo_ingreso'
+  match '/login' => 'public#login'
+  match '/registro' => 'public#registro'
+  match '/buscar' => 'public#buscar'
 
 end
