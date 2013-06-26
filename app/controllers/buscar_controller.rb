76d1249b598a82a2b7dbce41299ac32d020dcc96
daunillo <dauno.marcelo@gmail.com>
2013-06-26 08:47:40 +0200

    añadi la varialbe @usuario la que puede tenar el nombre o la primera parte del correo

--- app/controllers/buscar_controller.rb
+++ app/controllers/buscar_controller.rb
@@ -2,6 +2,14 @@ class BuscarController < ApplicationController
   def index
     @session = session
     @comunas = Comuna.for_select
+    usuario =  Usuario.find(session[:usuario_id])
+	  if usuario.nombre.blank?
+	    cortado=usuario.correo_electronico
+	    cortado=cortado.split("@")
+      	    @usuario = cortado[0]
+	  else
+	    @usuario = usuario.nombre
+	  end
   end
 
   def nueva
