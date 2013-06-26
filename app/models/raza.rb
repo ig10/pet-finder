6b9cf11ad5728678691292ed719a9158aba44a1e
Ignacio Diez <ig.diez10@gmail.com>
2013-06-26 03:35:07 -0400

    Raza: Opciones para selectores

--- app/models/raza.rb
+++ app/models/raza.rb
@@ -1,4 +1,11 @@
 class Raza < ActiveRecord::Base
   has_many :mascota
   attr_accessible :nombre, :tipo
+
+  Tipos = [["Gato", "gato"], ["Perro", "perro"]]
+
+  def self.for_select
+    self.order('nombre ASC').map{|r| [r.nombre, r.id]}
+  end
+
 end
