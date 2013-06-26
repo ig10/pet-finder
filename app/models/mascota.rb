e99783ca107f5618209256c7f3c0caebc252214a
Ignacio Diez <ig.diez10@gmail.com>
2013-06-26 03:33:18 -0400

    Mascota: Relacion con usuario + arreglos para selectores

--- app/models/mascota.rb
+++ app/models/mascota.rb
@@ -1,6 +1,12 @@
 class Mascota < ActiveRecord::Base
   belongs_to :raza
+  has_one :usuario
+
   attr_accessible :nombre, :color, :edad, :genero, :tamano, :estado, :raza_id
   delegate :tipo, to: :raza
+  delegate :comuna_id, to: :usuario
+
+  Generos = [["Macho","macho"],["Hembra","hembra"],["Desconocido","desconocido"]]
+  Edades = [["Bebe","bebe"],["Joven","joven"],["Adulto","adulto"],["Mayor","mayor"]]
 
 end
