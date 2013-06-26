7a9e15f02f94e9a927f39f40840022fb4c5c991c
Ignacio Diez <ig.diez10@gmail.com>
2013-06-26 03:32:38 -0400

    Comuna: Relacion con usuario

--- app/models/comuna.rb
+++ app/models/comuna.rb
@@ -1,6 +1,7 @@
 class Comuna < ActiveRecord::Base
   belongs_to :provincia
   belongs_to :ciudad
+  has_many :usuario
 
   validates_presence_of :provincia_id, :codigo_ine, :nombre
   attr_accessible :provincia_id, :codigo_ine, :nombre
