63f0234eb0e9135ea46a5ce418cbcb5ae05bfcdd
Ignacio Diez <ig.diez10@gmail.com>
2013-06-26 03:32:21 -0400

    Buscar: Se cambian valores de selectores en duro por variables

--- app/controllers/buscar_controller.rb
+++ app/controllers/buscar_controller.rb
@@ -2,8 +2,14 @@ class BuscarController < ApplicationController
   def index
     @session = session
     @comunas = Comuna.for_select
+    @tipos = Raza::Tipos
+    @razas = Raza.for_select
+    @generos = Mascota::Generos
+    @edades = Mascota::Edades
   end
 
   def nueva
+    @mascotas = Mascota.where(params[:mascota])
+    render partial: 'resultados', locals: @mascotas
   end
 end
